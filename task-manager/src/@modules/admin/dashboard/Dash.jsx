c7896a811d5ef1b283f008ad4f57fffa82fe4a00
Симеон Мечков <montishak@gmail.com>
2020-04-24 12:57:54 +0300

    initial dashboards for client and admin

---
+++ task-manager/src/@modules/admin/dashboard/Dash.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Dash.scss';
+
+export default class AdminDash extends React.Component {
+    // constructor() {
+    //     super();
+
+    // }
+
+    render() {
+        return (
+            <div className="admin-dash">
+
+            </div>
+        );
+    }
+}
---
+++ task-manager/src/@modules/user/dashboard/Dash.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Dash.scss';
+
+export default class UserDash extends React.Component {
+    // constructor() {
+    //     super();
+    // }
+
+    render() {
+        return (
+            <div className="user-dash">
+            </div>
+        );
+    }
+}
